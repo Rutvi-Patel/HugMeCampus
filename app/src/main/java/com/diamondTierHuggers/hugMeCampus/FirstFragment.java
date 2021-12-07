068d058620fc444c81b076a83a2c2ac9459dc081
rithsuon <rithrangsey.suon@student.csulb.edu>
2021-12-06 19:53:21 -0800

    Added User Input

--- app/src/main/java/com/diamondTierHuggers/hugMeCampus/FirstFragment.java
+++ app/src/main/java/com/diamondTierHuggers/hugMeCampus/FirstFragment.java
@@ -4,17 +4,27 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
 import com.diamondTierHuggers.hugMeCampus.databinding.FragmentFirstBinding;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class FirstFragment extends Fragment {
 
     private FragmentFirstBinding binding;
 
+    String input;
+
+    EditText inputField;
+
+    Button button;
     @Override
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container,
@@ -28,6 +38,19 @@ public class FirstFragment extends Fragment {
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance("https://hugmecampus-dff8c-default-rtdb.firebaseio.com/");
+        DatabaseReference myRef = database.getReference("UserInput");
+
+
+
+        inputField = (EditText) view.findViewById(R.id.userInput);
+        binding.sendDBButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                myRef.setValue(inputField.getText().toString());
+            }
+        });
 
         binding.buttonFirst.setOnClickListener(new View.OnClickListener() {
             @Override
